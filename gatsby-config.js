4f62c01867b42dd1dcdfac80cd8b60d2fce134eb
lolmc00 <lolmc0805@gmail.com>
2019-12-27 22:37:09 +0900

    개츠비 feed 플러그인 오류로 인해서 해제

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -73,6 +72,5 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    `gatsby-plugin-material-ui`,
   ],
 }
