89598f8fff9634f886b9e60dbf2e24ea342dc01a
lolmc00 <lolmc0805@gmail.com>
2019-12-27 21:31:32 +0900

    헤더 배너 컴포넌트 추가

---
+++ src/components/banner.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+const Banner = (props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      banner: file(absolutePath: { regex: "/banner.jpg/" }) {
+        childImageSharp {
+          fixed(width: 1960, quality: 100) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return(
+    <div style={{
+    }}>
+      <Img
+        fixed={data.banner.childImageSharp.fixed}
+        alt={"banner"}
+        style={{
+          width: "100%",
+          height: "500px",
+          borderBottom: "2px solid #000"
+        }}
+      />
+    </div>
+  )
+}
+
+export default Banner
