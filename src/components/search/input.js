9349880ae9dcebad040fd712121acbc360390700
lolmc00 <lolmc0805@gmail.com>
2019-12-31 03:35:43 +0900

    input box 디자인 수정

--- src/components/search/input.js
+++ src/components/search/input.js
@@ -1,16 +1,38 @@
 import React from "react"
 import { connectSearchBox } from "react-instantsearch-dom"
+import {Search} from "styled-icons/fa-solid/Search";
 
-
-export default connectSearchBox(({ refine, ...rest }) => (
-  <form style={{margin: "0"}}>
+export default connectSearchBox(({ refine, ...rest }) => {
+  
+  return(
+  <div style={{
+    margin: "0",
+    width: rest.focus ? "180px" : "120px",
+    height: "35px",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    transition: "0.3s ease all",
+    backgroundColor: "#eee",
+    }}>
+    <Search style={{
+      display: "inline-block",
+      width: "15px",
+      margin: "0px 8px",
+    }}/>
     <input
       type="text"
       placeholder="Search"
       aria-label="Search"
       onChange={e => refine(e.target.value)}
-      style={{height: "35px"}}
+      style={{
+        display: "inline-block",
+        border: "none",
+        outline: "none",
+        width: "70px",
+        backgroundColor: "#eee",
+      }}
       {...rest}
     />
-  </form>
-))
+  </div>);
+})
