1a8d9a74da171fa7b79bca7083d396d707d2e2a7
lolmc00 <lolmc0805@gmail.com>
2020-01-01 02:14:06 +0900

    백그라운드 이미지 설정

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,35 +1,58 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { Link, graphql, StaticQuery } from 'gatsby'
 
-import { rhythm } from "../utils/typography"
+import styled from 'styled-components'
 
-const Banner = (props) => {
-  const data = useStaticQuery(graphql`
-    query {
-      banner: file(absolutePath: { regex: "/banner.jpg/" }) {
-        childImageSharp {
-          fixed(width: 1960, quality: 100) {
-            ...GatsbyImageSharpFixed
+import BackgroundImage from 'gatsby-background-image'
+
+const StyledBackground = styled(BackgroundImage)`
+  height: 380px;
+  &::before,
+  &::after {
+    opacity:0.6 !important;
+    filter:alpha(opacity=60);
+  }
+  textDecoration: none;
+`
+
+const Banner = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "banner.jpg" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
           }
         }
       }
-    }
-  `)
-  return(
-    <div style={{
-    }}>
-      <Img
-        fixed={data.banner.childImageSharp.fixed}
-        alt={"banner"}
-        style={{
-          width: "100%",
-          height: "380px",
-          borderBottom: "2px solid #000"
-        }}
-      />
-    </div>
-  )
-}
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <StyledBackground
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          style={{backgroundColor: "#000", borderBottom: "2px #000 solid"}}
+        >
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            height: "350px",
+          }}>
+            <Link to="/" style={{boxShadow: "none"}}><h1 style={{fontSize: "40px", color:"#fff"}}>lolmc's Development Blog</h1></Link>
+          </div>
+          <div style={{textAlign: "right"}}>
+            <a href="http://www.freepik.com" style={{color: "#fff", boxShadow: "none", margin: "0"}}>Designed by vectorpouch / Freepik</a>
+          </div>
+        </StyledBackground>
+      )
+    }}
+  />
+)
 
 export default Banner
